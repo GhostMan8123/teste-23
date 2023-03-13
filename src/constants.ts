a4400726fbc145c75e70919c3ac120569a893d09
GhostMan8123 <127138855+GhostMan8123@users.noreply.github.com>
2023-03-13 16:55:17 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
-export const HOST = "server-sa.bombcrypto.io";
+export const HOST = "server-sea.bombcrypto.io";
 export const PORT = 443;
 export const ZONE = "BomberGameZone";
 export const SALT_KEY = "f17e4e44f7bbc229fdc2d2f55728abba";
 export const DATE_OFFSET = 62135596800000;
-export const VERSION_CODE = 22120915;
+export const VERSION_CODE = 23030711;
 export const SERVERS = ["na", "sea", "sa"];
 export const LC = "";
 
